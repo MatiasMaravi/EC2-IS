aa6469eb62a563dd08b5f571bc0b8fd26c1d6a38
JLeandroJM <jose.machaca.s@utec.edu.pe>
2023-10-19 20:36:58 -0500

    sin funcion

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
-from flask import Flask
+from flask import Flask, request, jsonify,render_template
+from utils.calcular import calcular_distancia
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '¡Hola, mundo!'
+    return render_template('index.html')
+
+
+@app.route('/calcular_distancia', methods=['POST'])
+def calcular_distancia_route():
+    city1 = request.form.get('city1')
+    country1 = request.form.get('country1')
+    city2 = request.form.get('city2')
+    country2 = request.form.get('country2')
+    metodo = request.form.get('metodo')
+
+    distancia = calcular_distancia(city1, country1, city2, country2,metodo)
+
+    return jsonify({'distancia': distancia})
 
 if __name__ == '__main__':
     app.run()
---
+++ utils/calcular.py
@@ -0,0 +1,8 @@
+# from geopy.distance import geodesic
+
+def calcular_distancia(ciudad1, pais1, ciudad2, pais2,metodo):
+    ubicacion1 = f"{ciudad1}, {pais1}"
+    ubicacion2 = f"{ciudad2}, {pais2}"
+
+    # distancia = geodesic(ubicacion1, ubicacion2).kilometers
+    return 100.0
